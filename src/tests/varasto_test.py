409c3872ce9fe971a5a98915c153a6aa85eda9f0
Fialco <113672867+Fialco@users.noreply.github.com>
2024-10-29 14:37:58 +0200

    ex 8 done

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,49 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+
+    ### tehtävä 8. Unittest 
+    def test_uudella_varastolla_negatiivinen_tilavuus(self):
+        self.varasto = Varasto(-2)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_uudella_varastolla_negatiivinen_saldo(self):
+        self.varasto = Varasto(10, -2)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_lisaa_saldoa_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(-8)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_lisaa_saldoa_yli_tilavuuden(self):
+        self.varasto.lisaa_varastoon(12)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ottaminen_negatiivisella_maaralla_ei_ota_varastosta(self):
+        self.varasto.lisaa_varastoon(8)
+
+        saatu_maara = self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_yli_ottaminen_tyhjentää_varaston_oikein(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.ota_varastosta(10)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_yli_ottaminen_saatu_maara_oikein(self):
+        self.varasto.lisaa_varastoon(8)
+
+        saatu_maara = self.varasto.ota_varastosta(10)
+
+        self.assertAlmostEqual(saatu_maara, 8)
+
+    def test_tulostaa_oikein(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.assertEqual(str(self.varasto), "saldo = 8, vielä tilaa 2")
